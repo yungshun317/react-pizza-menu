b778e2694304bd2a5320349103c489fb5a8bdad7
yungshun317 <seraph317@gmail.com>
2024-01-07 22:05:44 +0800

    [FEAT] Create the header component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+
+function App() {
+    return (
+        <div className="container">
+            <Header />
+            <Menu />
+            <Footer />
+        </div>
+    );
+}
+
+function Header() {
+    const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+
+    return (
+        <header className="header">
+            <h1 style={style}>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Menu() {
+
+}
+
+function Footer() {
+}
