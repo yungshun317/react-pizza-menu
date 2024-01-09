c2d5ca264c7a4ad3e11eb3e5d92248e1a32fbc45
yungshun317 <seraph317@gmail.com>
2024-01-09 10:16:02 +0800

    [FEAT] Use `Fragment` in the menu component and destructure the passing props to use only the named properties

--- src/index.js
+++ src/index.js
@@ -2,12 +2,56 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+const pizzaData = [
+    {
+        name: "Focaccia",
+        ingredients: "Bread with italian olive oil and rosemary",
+        price: 6,
+        photoName: "pizzas/focaccia.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Margherita",
+        ingredients: "Tomato and mozarella",
+        price: 10,
+        photoName: "pizzas/margherita.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Spinaci",
+        ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+        price: 12,
+        photoName: "pizzas/spinaci.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Funghi",
+        ingredients: "Tomato, mozarella, mushrooms, and onion",
+        price: 12,
+        photoName: "pizzas/funghi.jpg",
+        soldOut: false,
+    },
+    {
+        name: "Pizza Salamino",
+        ingredients: "Tomato, mozarella, and pepperoni",
+        price: 15,
+        photoName: "pizzas/salamino.jpg",
+        soldOut: true,
+    },
+    {
+        name: "Pizza Prosciutto",
+        ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+        price: 18,
+        photoName: "pizzas/prosciutto.jpg",
+        soldOut: false,
+    },
+];
+
 function App() {
     return (
         <div className="container">
             <Header />
             <Menu />
-            <Footer />
         </div>
     );
 }
@@ -23,8 +67,75 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = pizzaData;
+    const numPizzas = pizzas.length;
 
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+
+            { numPizzas > 0 ? (
+                <>
+                    <p>
+                        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.
+                    </p>
+                    <ul className="pizzas">
+                        {pizzas.map((pizza) => (
+                            <Pizza pizzaObj={pizza} key={pizza.name} />
+                        ))}
+                    </ul>
+                </>
+            ) : (
+                <p>We're still working on our menu. Please come back later.</p>
+            )}
+
+            { /*
+                <Pizza
+                    name="Pizza Spinaci"
+                    ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+                    photoName="pizzas/spinaci.jpg"
+                    price={10}
+                />
+                <Pizza
+                    name="Pizza Funghi"
+                    ingredients="Tomato, mushrooms"
+                    price={12}
+                    photoName="pizzas/funghi.jpg"
+                />
+            */ }
+        </main>
+    );
 }
 
 function Footer() {
 }
+
+function Pizza ({ pizzaObj }) {
+    console.log(pizzaObj);
+
+    return (
+        <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
+            <img src={pizzaObj.photoName} alt={pizzaObj.name} />
+            <div>
+                <h3>{pizzaObj.name}</h3>
+                <p>{pizzaObj.ingredients}</p>
+                {
+                    pizzaObj.soldOut ? (
+                        <span>SOLD OUT</span>
+                    ): (
+                        <span>{pizzaObj.price}</span>
+                    )
+                }
+                <span>{pizzaObj.soldOut ? "SOLD OUT" : pizzaObj.price}</span>
+            </div>
+        </li>
+    )
+}
+
+// React v18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
